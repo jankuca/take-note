c9afe895d77e0bab24bb4d274d6c999a911be9af
Jan Kuča <jan@jankuca.com>
2012-02-26 16:18:42 +0100

    drawing plugin: do not save layers

--- src/plugins.js
+++ src/plugins.js
@@ -12,28 +12,9 @@ takeNote.Plugins = {};
 takeNote.Plugins['drawing'] = function (data) {
 	var xml = '';
 	var state = data.plugin_state;
-	state.forEach(function (layer, i) {
-		if (i === 0) {
-			// The first item is the final merged image
-			xml += '<![CDATA[' + layer['data'].match(/;base64,(.*)/)[1] + ']]>';
-			xml += '<drawing>';
-		} else {
-			xml += '<layer';
-			if (layer['color']) {
-				xml += ' color="' + layer['color'] + '"';
-			}
-			var data = layer['data'];
-			if (data) {
-				var mime = data.match(/^data:([\w-\/]+);base64,(.*)/);
-				xml += ' type="' + mime[1] + '">';
-				xml += '<![CDATA[' + mime[2] + ']]>'
-				xml += '</layer>';
-			} else {
-				xml += ' />';
-			}
-		}
-	});
-	xml += '</drawing>';
+	var layer = state[0];
+	// The first item is the final merged image
+	xml += '<![CDATA[' + layer['data'].match(/;base64,(.*)/)[1] + ']]>';
 
 	return xml;
 };
